42f157fff9cff678ea132a1917f619d783e24447
grochmal <mike@grochmal.org>
2024-06-20 04:27:34 +0100

    feat: step 1 of pipeline

--- honeycrisp/buy_apples.py
+++ honeycrisp/buy_apples.py
@@ -1,12 +1,9 @@
-from typing import Annotated, Optional
+from typing import Annotated
 
-import mlflow
 import pandas as pd
 import typer
 from tqdm import tqdm
 from transformers.pipelines import Pipeline, pipeline
-
-from honeycrisp.core import start_run
 
 ZERO_SHOT_TASK = "zero-shot-classification"
 ZERO_SHOT_MODEL = "facebook/bart-large-mnli"
@@ -31,32 +28,21 @@ def emails_to_purchases(
 
 
 def buy_apples(
-    experiment: str,
     emails_from_farmers: Annotated[str, "Input"],
     apple_types: Annotated[str, "Input"],
     rejected_mail: Annotated[str, "Output"],
     apples_in_basement: Annotated[str, "Output"],
-    run_name: Optional[str] = None,
-    tracking_uri: str = "https://dagshub.com/grochmal/mlops-workshop.mlflow",
 ) -> None:
     zero_shot = pipeline(ZERO_SHOT_TASK, model=ZERO_SHOT_MODEL)
-    with start_run(tracking_uri=tracking_uri, experiment=experiment, run_name=run_name):
-        emails_df = pd.read_parquet(emails_from_farmers)
-        emails = emails_df["emails"]
-        mlflow.log_param("number_of_emails", len(emails))
-        apple_types_df = pd.read_parquet(apple_types)
-        apple_types = list(apple_types_df["apple_types"])
-        mlflow.log_param("apple_types", len(apple_types))
-        # Validate outputs before doing any work
-        pd.DataFrame().to_parquet(rejected_mail)
-        pd.DataFrame().to_parquet(apples_in_basement)
+    emails_df = pd.read_parquet(emails_from_farmers)
+    emails = emails_df["emails"]
+    apple_types_df = pd.read_parquet(apple_types)
+    apple_types = list(apple_types_df["apple_types"])
 
-        rejected_emails, basement = emails_to_purchases(emails, apple_types, zero_shot)
+    rejected_emails, basement = emails_to_purchases(emails, apple_types, zero_shot)
 
-        mlflow.log_metric("rejected_emails", len(rejected_emails))
-        pd.DataFrame({"emails": rejected_emails}).to_parquet(rejected_mail)
-        mlflow.log_metric("apples_in_basement", len(basement))
-        pd.DataFrame({"apples_in_basement": basement}).to_parquet(apples_in_basement)
+    pd.DataFrame({"emails": rejected_emails}).to_parquet(rejected_mail)
+    pd.DataFrame({"apples_in_basement": basement}).to_parquet(apples_in_basement)
 
 
 if __name__ == "__main__":
